a7eb0e34c7800b8f1cf7c4be792e04279982dfce
dharmendra1234 <dharmendra.pawar@gmail.com>
2017-06-18 15:13:12 -0500

    Checked-in files for React helloworld project

---
+++ my-app/src/App.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class Header extends React.Component {
+   render() {
+      return (
+         <div>
+            <h1>ReactJS is Awesome!!!</h1>
+         </div>
+      );
+   }
+}
+
+class Content extends React.Component {
+   render() {
+      return (
+         <div>
+            <h2>
+
+
+              React allows to create the DOM objects
+            </h2>
+
+         </div>
+      );
+   }
+}
+class App extends Component {
+  render() {
+    return (
+
+      <div className="App">
+        <div>
+                   <Header/>
+                   <Content/>
+                </div>
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>GOD IS GREAT !!!</h2>
+        </div>
+
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ my-app/src/App2.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App2 extends Component {
+  render() {
+    return (
+      <div className="App2">
+      <h4> Hello </h4>
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>GOD IS GREAT 5!!</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App2;
